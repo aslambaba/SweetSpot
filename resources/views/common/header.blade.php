0200fa518a275f03919ec417739b7ce7a95ef84b
Aslam Sarfraz <aslam91r@gmail.com>
2021-03-12 15:07:38 +0500

    Header Section Added!!

---
+++ resources/views/common/header.blade.php
@@ -0,0 +1,17 @@
+<div class='HeaderMain'>
+    <div class="brandName">
+        <h2>SweetSpot</h2>
+    </div>
+    <div class="HeaderMenu">
+        <ul>
+            <li><a href='/sweetspot/public/'>Home</a></li>
+            <li><a href='/sweetspot/public/products'>Products</a></li>
+            <li><a href='/sweetspot/public/contactus'>Contact Us</a></li>
+            <li>
+                <button>
+                    Admin Login
+                </button>
+            </li>
+        </ul>
+    </div>
+</div>
